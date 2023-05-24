1302dac33ac0196deae1fbc5cdf774bdc106be57
poojajayasri <130843120+poojajayasri@users.noreply.github.com>
2023-05-24 19:15:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,12 @@
 from flask import Flask, jsonify, request, render_template
-import os
 from langchain.llms import OpenAI
 from flask_cors import CORS
-
+openai_api_key = "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"
 llm = OpenAI(model_name="text-ada-001", openai_api_key=openai_api_key)
 
 
 
 app = Flask(__name__)
-
-# Set API keys as environment variables
-os.environ["OPENAI_API_KEY"] = "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"
-
-
 
 # Flask routes
 @app.route("/")
